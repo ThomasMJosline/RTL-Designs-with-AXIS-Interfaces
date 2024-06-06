adf25e42dc093ff904318ff156269315edebaee8
ThomasMJosline <84652232+ThomasMJosline@users.noreply.github.com>
2024-06-06 12:56:36 +0530

    Update main module

--- MUX with AXIS interfaces/AXI_mux.v
+++ MUX with AXIS interfaces/AXI_mux.v
@@ -5,8 +5,8 @@ module AXI_mux(
     input sel,
     output reg [7:0] DATA_out,
 
-    input TVALID_in_1, TVALID_in_2,
-    input TLAST_in_1, TLAST_in_2,
+    input TVALID_in_0, TVALID_in_1,
+    input TLAST_in_0, TLAST_in_1,
     output TREADY_in,
 
     input TREADY_out,
@@ -28,17 +28,17 @@ always @(posedge ACLK or negedge ARESETn) begin
         TLAST_out = 0;
         if (TREADY_in) begin
             if (sel==0) begin
-                if (TVALID_in_1 == 1) begin
+                if (TVALID_in_0 == 1) begin
                     DATA_out = a;
                     TVALID_out = 1;
-                    TLAST_out = TLAST_in_1;
+                    TLAST_out = TLAST_in_0;
                 end
             end
             else begin
-                if (TVALID_in_2 == 1) begin
+                if (TVALID_in_1 == 1) begin
                     DATA_out = b;
                     TVALID_out = 1;
-                    TLAST_out = TLAST_in_2;
+                    TLAST_out = TLAST_in_1;
                 end
             end
         end
