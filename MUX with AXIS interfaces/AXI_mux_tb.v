93157520d17a294a44b99838cbc438336f65c4e6
ThomasMJosline <84652232+ThomasMJosline@users.noreply.github.com>
2024-06-10 11:26:32 +0530

    Update testbench

--- MUX with AXIS interfaces/AXI_mux_tb.v
+++ MUX with AXIS interfaces/AXI_mux_tb.v
@@ -1,90 +1,87 @@
 `timescale 1ns/1ps
 module AXI_mux_tb;
 
-
-reg ACLK;
-reg ARESETn;
-reg [7:0] DATA_in_0, DATA_in_1;
+reg aclk;
+reg aresetn;
+reg [7:0] s_axis_tdata_0, s_axis_tdata_1;
 reg sel;
-reg TVALID_in_0, TVALID_in_1;
-reg TLAST_in_0, TLAST_in_1;
-reg TREADY_out;
+reg s_axis_tvalid_0, s_axis_tvalid_1;
+reg s_axis_tlast_0, s_axis_tlast_1;
+reg s_axis_tready;
 
-wire TREADY_in;
-wire [7:0] DATA_out;
-wire TVALID_out; 
-wire TLAST_out ;
-
+wire m_axis_tready;
+wire [7:0] m_axis_tdata;
+wire m_axis_tvalid; 
+wire m_axis_tlast ;
 
 reg [3:0] count_0;
 reg [4:0] count_1;
 
 
 
-     AXI_mux mux_dut (.ACLK(ACLK), .ARESETn(ARESETn), .DATA_in_0(DATA_in_0), .DATA_in_1(DATA_in_1), .sel(sel), .DATA_out(DATA_out),
-.TVALID_in_0(TVALID_in_0), .TVALID_in_1(TVALID_in_1), .TLAST_in_0(TLAST_in_0), .TLAST_in_1(TLAST_in_1),
-.TREADY_in(TREADY_in), .TREADY_out(TREADY_out), .TVALID_out(TVALID_out), .TLAST_out(TLAST_out));
+AXI_mux mux_dut (.aclk(aclk), .aresetn(aresetn), .s_axis_tdata_0(s_axis_tdata_0), .s_axis_tdata_1(s_axis_tdata_1), .sel(sel), .m_axis_tdata(m_axis_tdata),
+.s_axis_tvalid_0(s_axis_tvalid_0), .s_axis_tvalid_1(s_axis_tvalid_1), .s_axis_tlast_0(s_axis_tlast_0), .s_axis_tlast_1(s_axis_tlast_1),
+.m_axis_tready(m_axis_tready), .s_axis_tready(s_axis_tready), .m_axis_tvalid(m_axis_tvalid), .m_axis_tlast(m_axis_tlast));
 
 
-always #5 ACLK = ~ACLK;
+always #5 aclk = ~aclk;
 
 initial begin
-     ACLK=0;
-     ARESETn = 1; 
-     DATA_in_0 = 0; TVALID_in_0 = 0; TLAST_in_0 = 0; count_0 = 0;
-     DATA_in_1 = 64; TVALID_in_1 = 0; TLAST_in_1 = 0; count_1 = 0;
+     aclk=0;
+     aresetn = 1; 
+     s_axis_tdata_0 = 0; s_axis_tvalid_0 = 0; s_axis_tlast_0 = 0; count_0 = 0;
+     s_axis_tdata_1 = 64; s_axis_tvalid_1 = 0; s_axis_tlast_1 = 0; count_1 = 0;
      sel = 0;
-     TREADY_out = 0;
+     s_axis_tready = 0;
 
-    #5 ARESETn = 0;
+    #5 aresetn = 0;
 
-    #5 ARESETn = 1;
-
-
-    #5 TVALID_in_0 =0 ; TVALID_in_1 =0 ; TREADY_out =1 ; sel =1;
-
-    
-    #5 TVALID_in_0 =1 ; TVALID_in_1 =0 ; TREADY_out =1 ; sel = 0 ;
-
-    
-    #5 TVALID_in_0 =1 ; TVALID_in_1 =0 ; TREADY_out =0 ; sel =0 ;
+    #5 aresetn = 1;
 
 
-    #5 TVALID_in_0 =0 ; TVALID_in_1 =1 ; TREADY_out =0 ; sel =1 ;
+    #10 s_axis_tvalid_0 =0 ; s_axis_tvalid_1 =0 ; s_axis_tready =1 ; sel =0;
 
+    #10 s_axis_tvalid_0 =1 ; s_axis_tvalid_1 =0 ; s_axis_tready =1 ; sel = 0 ;
 
-    #5 TVALID_in_0 =0 ; TVALID_in_1 =1 ; TREADY_out =1 ; sel =1 ;
+    #10 s_axis_tvalid_0 =0 ; s_axis_tvalid_1 =0 ; s_axis_tready =1 ; sel =0 ;
 
-    
-    #5 TVALID_in_0 =1 ; TVALID_in_1 =1 ; TREADY_out =1 ; sel =0 ;
+    #10 s_axis_tvalid_0 =1 ; s_axis_tvalid_1 =0 ; s_axis_tready =0 ; sel =0 ;
 
-    
-    #5 TVALID_in_0 =1 ; TVALID_in_1 =1 ; TREADY_out =0 ; sel =1 ;
+    #10 s_axis_tvalid_0 =1 ; s_axis_tvalid_1 =0 ; s_axis_tready =1 ; sel =0 ;
 
-    
-    #5 TVALID_in_0 =1 ; TVALID_in_1 =0 ; TREADY_out =1 ; sel =0 ;
+    #10 s_axis_tvalid_0 =1 ; s_axis_tvalid_1 =1 ; s_axis_tready =0 ; sel =0 ;
+
+    #10 s_axis_tvalid_0 =0 ; s_axis_tvalid_1 =1 ; s_axis_tready =0 ; sel =1 ;
+
+    #10 s_axis_tvalid_0 =0 ; s_axis_tvalid_1 =1 ; s_axis_tready =1 ; sel =1 ;
+
+    #10 s_axis_tvalid_0 =0 ; s_axis_tvalid_1 =0 ; s_axis_tready =1 ; sel =1 ;
+
+    #10 s_axis_tvalid_0 =0 ; s_axis_tvalid_1 =1 ; s_axis_tready =1 ; sel =1 ;
+
+    #10 s_axis_tvalid_0 =1 ; s_axis_tvalid_1 =1 ; s_axis_tready =1 ; sel =0 ;
 
 
 end
 
-always @(posedge ACLK) begin
-    DATA_in_0 <= $random;
-    DATA_in_1 <= $random;
+always @(posedge aclk) begin
+    s_axis_tdata_0 <= $random;
+    s_axis_tdata_1 <= $random;
     if (count_0 == 4'd7) begin
-        TLAST_in_0 <= 1;
+        s_axis_tlast_0 <= 1;
         count_0 <= 0;
     end
     else begin
-        TLAST_in_0 <= 0;
+        s_axis_tlast_0 <= 0;
         count_0 <= count_0 + 1'b1;
     end
 
     if (count_1 == 4'd15) begin
-        TLAST_in_1 <= 1;
+        s_axis_tlast_1 <= 1;
         count_1 <= 0;
     end
     else begin
-        TLAST_in_1 <= 0;
+        s_axis_tlast_1 <= 0;
         count_1 <= count_1 + 1'b1;
     end
 end
