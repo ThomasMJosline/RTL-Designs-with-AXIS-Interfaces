c81778f8712a4b9db763b1f18f4c75ff53418c12
ThomasMJosline <84652232+ThomasMJosline@users.noreply.github.com>
2024-06-10 11:25:24 +0530

    Update main module

--- MUX with AXIS interfaces/AXI_mux.v
+++ MUX with AXIS interfaces/AXI_mux.v
@@ -1,47 +1,47 @@
 module AXI_mux(
-    input ACLK,
-    input ARESETn,
-    input [7:0] DATA_in_0, //! input data
-    input [7:0] DATA_in_1, //! input data
+    input aclk,
+    input aresetn,
+    input [7:0] s_axis_tdata_0, //! input data
+    input [7:0] s_axis_tdata_1, //! input data
     input sel,     //! select
-    output reg [7:0] DATA_out, //!output data
+    output reg [7:0] m_axis_tdata, //!output data
 
-    input TVALID_in_0,  // 
-    input TVALID_in_1,
-    input TLAST_in_0, 
-    input TLAST_in_1,
-    output TREADY_in,
+    input s_axis_tvalid_0,  //! VALID signal for s_axis_tdata_0
+    input s_axis_tvalid_1,  //! VALID signal for s_axis_tdata_1
+    input s_axis_tlast_0,  //! LAST signal for s_axis_tdata_0
+    input s_axis_tlast_1,  //! LAST signal for s_axis_tdata_0
+    output s_axis_tready,  //! signal that shows the slave(FIFO) is ready to accept data
 
-    input TREADY_out,
-    output reg TVALID_out, 
-    output reg TLAST_out 
+    input m_axis_tready,   //! signal that shows the master is ready accept data
+    output reg m_axis_tvalid, //! VALID signal for output data (m_axis_tdata)
+    output reg m_axis_tlast   //! LAST signal for the output data
 );
 
 
-always @(posedge ACLK or negedge ARESETn) begin
-    if (!ARESETn) begin
-        DATA_out = 0;
-        TVALID_out = 0;
-        TLAST_out = 0;
+always @(posedge aclk or negedge aresetn) begin
+    if (!aresetn) begin
+        m_axis_tdata = 0;
+        m_axis_tvalid = 0;
+        m_axis_tlast = 0;
     end
     else begin
         // Default assignments
-        DATA_out = 0;
-        TVALID_out = 0;
-        TLAST_out = 0;
-        if (TREADY_in) begin
+        m_axis_tdata = 0;
+        m_axis_tvalid = 0;
+        m_axis_tlast = 0;
+        if (s_axis_tready) begin
             if (sel==0) begin
-                if (TVALID_in_0 == 1) begin
-                    DATA_out = DATA_in_0;
-                    TVALID_out = 1;
-                    TLAST_out = TLAST_in_0;
+                if (s_axis_tvalid_0 == 1) begin
+                    m_axis_tdata = s_axis_tdata_0;
+                    m_axis_tvalid = 1;
+                    m_axis_tlast = s_axis_tlast_0;
                 end
             end
             else begin
-                if (TVALID_in_1 == 1) begin
-                    DATA_out = DATA_in_1;
-                    TVALID_out = 1;
-                    TLAST_out = TLAST_in_1;
+                if (s_axis_tvalid_1 == 1) begin
+                    m_axis_tdata = s_axis_tdata_1;
+                    m_axis_tvalid = 1;
+                    m_axis_tlast = s_axis_tlast_1;
                 end
             end
         end
@@ -49,6 +49,6 @@ always @(posedge ACLK or negedge ARESETn) begin
 end
 
 
-assign TREADY_in = TREADY_out;
+assign s_axis_tready = m_axis_tready;
 
 endmodule
